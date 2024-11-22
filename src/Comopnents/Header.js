67aafd1aef0e47331cc01282109d4241ff7f8dc8
RakshithanrGowda <rakshithanr342@gmail.com>
2024-11-22 23:55:11 +0530

    second changes

---
+++ src/Comopnents/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Header = () => {
+  return (
+    <div>
+        <img src="/logo.png" alt='logo'/>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Comopnents/Login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './Header'
+
+
+const login = () => {
+  return (
+    <div>
+      <Header/>
+      <div>
+        <img src='/Backgroundimage.png' alt='background'/>
+      </div>
+    </div>
+  )
+}
+
+export default login
+
+
